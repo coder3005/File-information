40fbaee200d194ce7decc86d9e358ecaca9f573c
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2023-07-10 14:12:42 +0530

    Update fileInfo.java

--- fileInfo.java
+++ fileInfo.java
@@ -10,7 +10,7 @@ class fileInfo
         {
             System.out.println("File Name: "+f.getName());
             System.out.println("File Location: "+f.getAbsolutePath());
-            System.out.println("Fmile Writable: "+f.canWrite());
+            System.out.println("File Writable: "+f.canWrite());
             System.out.println("File Readable: "+f.canRead());
             System.out.println("File Size: "+f.length());
             System.out.println("File Removed: "+f.delete());
