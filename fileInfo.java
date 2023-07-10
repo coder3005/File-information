8ac2a0f5552f21553e12e584194472ea1921ee72
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2023-07-10 14:12:07 +0530

    Create fileInfo.java

---
+++ fileInfo.java
@@ -0,0 +1,23 @@
+/* File Information */
+import java.io.File;
+class fileInfo
+{
+    public static void main(String[] args) {
+        
+        File f=new File("C:\\Users\\Abhi Gupta\\downloads\\LC.txt");
+
+        if(f.exists())
+        {
+            System.out.println("File Name: "+f.getName());
+            System.out.println("File Location: "+f.getAbsolutePath());
+            System.out.println("Fmile Writable: "+f.canWrite());
+            System.out.println("File Readable: "+f.canRead());
+            System.out.println("File Size: "+f.length());
+            System.out.println("File Removed: "+f.delete());
+        }
+        else
+        {
+            System.out.println("File doesn't Exists");
+        }
+    }
+}
